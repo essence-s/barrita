6011ab59fbe42b34e197d11b1cbfabc29cf43a8e
styv <styv2301@gmail.com>
2026-04-24 09:25:34 -0500

    feat: se muestra el nombre del dia en español

--- src/app/time/common.rs
+++ src/app/time/common.rs
@@ -1,11 +1,22 @@
 use crate::core::data::TimeInfo;
-use chrono::Local;
+use chrono::{Local, Datelike};
 
 pub fn get_time_info() -> TimeInfo {
+    // let now = Local::now();
+
+    // let time = now.format("%-I:%M %p").to_string();
+    // let date = now.format("%a %m/%d").to_string();
+
+    // TimeInfo { time, date }
+
     let now = Local::now();
 
     let time = now.format("%-I:%M %p").to_string();
-    let date = now.format("%a %m/%d").to_string();
+
+    let dias = ["Dom", "Lun", "Mar", "Mié", "Jue", "Vie", "Sáb"];
+    let dia = dias[now.weekday().num_days_from_sunday() as usize];
+
+    let date = format!("{} {:02}/{:02}", dia, now.month(), now.day());
 
     TimeInfo { time, date }
 }
