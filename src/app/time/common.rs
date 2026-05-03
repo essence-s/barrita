c11ac55bd63cac9e20052aa3cda357a70b498cb4
styv <styv2301@gmail.com>
2026-05-03 13:01:05 -0500

    feat: se cambio el formato de fecha a español dia y luego mes

--- src/app/time/common.rs
+++ src/app/time/common.rs
@@ -16,7 +16,7 @@ pub fn get_time_info() -> TimeInfo {
     let dias = ["Dom", "Lun", "Mar", "Mié", "Jue", "Vie", "Sáb"];
     let dia = dias[now.weekday().num_days_from_sunday() as usize];
 
-    let date = format!("{} {:02}/{:02}", dia, now.month(), now.day());
+    let date = format!("{} {:02}/{:02}", dia, now.day(), now.month());
 
     TimeInfo { time, date }
 }
